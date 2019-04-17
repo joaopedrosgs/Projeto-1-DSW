ccb9a9325e7316147328d2601263c56a9065297e
Matheus Bortoleto <matheus.silva.ufscar@gmail.com>
2019-04-17 16:06:04 -0300

    Adição de servlet para remoção de site de vendas

--- src/org/dsw/control/SiteVendas/DeleteServlet.java
+++ src/org/dsw/control/SiteVendas/DeleteServlet.java
@@ -11,38 +11,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.SQLException;
 
-@WebServlet(name = "UpdateServlet", urlPatterns = "/site/update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet(name = "DeleteServlet", urlPatterns = "/site/delete")
+public class DeleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO: Redirecionamento para os casos de erro
         HttpSession session = request.getSession();
 
         if(Permissoes.isAdminSession(session)) {
             String userId = request.getParameter("user_id");
-            String url = request.getParameter("url");
-            String nome = request.getParameter("nome");
-            String telefone = request.getParameter("telefone");
 
             if(userId != null) {
                 SiteVendas site = SiteVendasDAO.get(Integer.parseInt(userId));
 
                 if(site != null) {
-                    if (url != null)
-                        site.setUrl(url);
-
-                    if (nome != null)
-                        site.setNome(nome);
-
-                    if (telefone != null)
-                        site.setTelefone(telefone);
-
-                    try {
-                        SiteVendasDAO.update(site);
-                    } catch (SQLException e) {
-                        // Erro ao atualizar o site
-                    }
+                    SiteVendasDAO.delete(site);
                 }
             }
         }
