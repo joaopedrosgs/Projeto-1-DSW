7232814e6a45820d97ac5c2a4b7800575d21de6e
João Pedro Silva <pedrosgregorio9@gmail.com>
2019-04-19 17:27:23 -0300

    atualizando local do banco de dados

--- src/org/dsw/dao/GenericDAO.java
+++ src/org/dsw/dao/GenericDAO.java
@@ -8,8 +8,9 @@ public class GenericDAO {
     protected static Connection getConnection() {
         try {
             Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection("jdbc:sqlite:/home/pedro/projeto-1-dsw.sqlite");
+            return DriverManager.getConnection("jdbc:sqlite:"+System.getProperty("user.home")+"/projeto-1-dsw.sqlite");
         } catch (ClassNotFoundException | SQLException e) {
+
             throw new RuntimeException(e);
         }
     }
