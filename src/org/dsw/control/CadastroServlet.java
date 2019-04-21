9db4cfa58cdccf14929ca56a97b40b6d2e61fac3
Rene Ferrante <rene.ferranten@gmail.com>
2019-04-21 15:31:01 -0300

    Add doPost to CadastroServlet

--- src/org/dsw/control/CadastroServlet.java
+++ src/org/dsw/control/CadastroServlet.java
@@ -1,4 +1,6 @@
 package org.dsw.control;
+import org.dsw.dao.UsuarioDAO;
+import org.dsw.model.Usuario;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -6,9 +8,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "CadastroServlet", urlPatterns = "/cadastro")
 public class CadastroServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+        String senha = request.getParameter("senha");
+
+        if (email != null && senha != null) {
+        	Senha senhaHash = new Senha(senha);
+
+        	try {
+        		UsuarioDAO.create(new Usuario(email, senhaHash.getHash()));
+
+                    return;
+                } catch (SQLException e) {
+                    // Erro ao cadastrar usuário
+                }
+        }
+	}
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/cadastro.jsp").forward(request, response);
