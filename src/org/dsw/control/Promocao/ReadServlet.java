58dcfed8510547b350e06c0574c3f2626aad5f71
Rene Ferrante <rene.ferranten@gmail.com>
2019-04-23 09:25:20 -0300

    ReadServlet for Promocao added

---
+++ src/org/dsw/control/Promocao/ReadServlet.java
@@ -0,0 +1,15 @@
+package org.dsw.control.Promocao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ReadPromocao")
+public class ReadServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Decidir como apresentar os dados
+    }
+}
