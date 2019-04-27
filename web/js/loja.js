f405643710ccea6918afde123e2a0c6aef84196e
João Pedro S. G. Silva <pedrosgregorio9@gmail.com>
2019-04-27 16:10:03 -0300

    Deletando projeto antigo

--- web/js/loja.js
+++
@@ -1,9 +0,0 @@
-var deleteButtons = document.getElementsByClassName('delete');
-
-for (var i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener('click', dismiss);
-}
-
-function dismiss(e) {
-    this.parentNode.classList.add('is-hidden');
-}
